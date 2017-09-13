bb01d3c992bf579cef13a843b492a026afff6daa
YukiIwamoto <yukiiwamoto@YukiIwamotonoMacBook-Pro.local>
2017-09-14 06:00:55 +0900

    isssues/#3 commit leakage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 TwitterCloneapp::Application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+  resources :tweets, only:[:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root to: "home#index"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
